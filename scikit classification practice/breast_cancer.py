4b9aeb4f25ee3c5aea29a1c6376d5a181001681a
Logan <loganm123@users.noreply.github.com>
2021-05-19 14:24:43 -0700

    Added cross-validation and graph to find best number of neighbors
    Added 5 fold cross validation and went through 1-50 number of neighbors to find the best possible number of neighbors to optimize the fit

--- scikit classification practice/breast_cancer.py
+++ scikit classification practice/breast_cancer.py
@@ -12,12 +12,14 @@ from sklearn import datasets
 #imports matplot
 import matplotlib.pyplot as plt
 
-#imports the KNeighborsClassifier from sklearn
+#imports the KNeighborsClassifier from sklearn and other important things
 from sklearn.neighbors import KNeighborsClassifier
 
 from sklearn.model_selection import train_test_split
 
 from sklearn.neighbors import KNeighborsClassifier
+
+from sklearn.model_selection import cross_val_score
 
 #info on the breast_cancer dataset https://scikit-learn.org/stable/datasets/toy_dataset.html
 #loads in the breast cancer data set and assigns it to b_cancer
@@ -27,11 +29,27 @@ b_cancer = datasets.load_breast_cancer()
 y = b_cancer.target
 #assigns the X to all the other data
 X = b_cancer.data
-
+#create a training set composed of a third of the data
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.333, random_state = 69, stratify = y)
 
-knn = KNeighborsClassifier(n_neighbors = 5)
 
-knn.fit(X_train,y_train)
+#create a range of k values from 1 - 50
+neighbors = list(range(1,51))
 
-print(knn.score(X_test,y_test))
+#empty list to hold cv scores
+cv_scores = []
+
+#perform five fold cross-validation on each k number of n_neighbors
+for k in neighbors:
+    #initialize the classifier at the number of neighbors
+    knn = KNeighborsClassifier(n_neighbors= k)
+    #compute the scores for that k neighbors with a five fold scoring off of accuracy
+    scores = cross_val_score(knn, X_train, y_train, cv = 5, scoring = "accuracy")
+    #add the mean of the scores to the cv_scores list
+    cv_scores.append(scores.mean())
+
+#plot neighbors against avg cv score to find the best number of neighbors for a k means clustering
+plt.plot(neighbors,cv_scores)
+plt.xlabel("Neighbors")
+plt.ylabel("CV Score Average (5 fold)")
+plt.show()
