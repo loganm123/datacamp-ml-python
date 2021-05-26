eb30e75fb3d495c45b811f98715405c0b937fe25
Logan <loganm123@users.noreply.github.com>
2021-05-26 14:29:10 -0700

    created graph showing t-SNE clustering

--- scikit practice/unsupervised.py
+++ scikit practice/unsupervised.py
@@ -24,6 +24,16 @@ samples, y = datasets.load_wine(return_X_y=True)
 #builds TSNE model
 model = TSNE(learning_rate=50)
 
+#creates features
 tsne_features = model.fit_transform(samples)
 
-print(tsne_features)
+#print(tsne_features)
+
+#select 0th feature
+xs = tsne_features[:,0]
+#select the first feature
+ys = tsne_features[:,1]
+#creates scatter plot and colors by each unique wine set
+plt.scatter(xs, ys, c = y)
+#shows the clusters
+plt.show()
