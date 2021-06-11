98086053abd51ec5d788a466cbf019514387cb09
Logan <loganm123@users.noreply.github.com>
2021-06-10 19:07:45 -0700

    Started a new project with training a CART on census data

---
+++ scikit practice/CART_Census_Data/CART_XGBoost_Practice.py
@@ -0,0 +1,10 @@
+#CART project
+#using XGBoost
+#train CART to learn whether an adult will have income greater than $50k based on census data
+
+
+#import pandas
+import pandas as pd
+
+#import train test split
+from sklearn.model_selection import train_test_split
