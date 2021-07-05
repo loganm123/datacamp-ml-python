ee5958afefddbabe173cafa64136e291827037f6
Logan <loganm123@users.noreply.github.com>
2021-07-05 09:32:52 -0700

    To do: Fix the xgboost error apply one hot encoder to integer stand ins

--- scikit_practice/CART_Census_Data/CART_XGBoost_Practice.py
+++ scikit_practice/CART_Census_Data/CART_XGBoost_Practice.py
@@ -3,17 +3,28 @@
 #train CART to learn whether an adult will have income greater than $50k based on census data
 
 
-#fix the value error from the y_train data... I formatted it wrong somehow
-#fixing value error by changing to binary values for things like workign class instead of objects
-#labelencoder
-# In[cell1]
+#to do
+#fix the xgbboost error
+#apply one hot encoder to the integer stand ins
+
+# %%
 #import pandas
 import pandas as pd
 
 #import train test split
 from sklearn.model_selection import train_test_split
 
+#import label encoder so we can setup the data for xgboost
+from sklearn.preprocessing import LabelEncoder
 
+#imports OneHotEncoder so we can get rid of the natural ordering that results in label encoding
+#so the XGBoost classifier is not biased, e.g. education level 9 is better than 5
+from sklearn.preprocessing import OneHotEncoder
+
+#create a label encoder
+le = LabelEncoder()
+
+# %%
 #setting the right working directory
 import os
 print(os.getcwd())
@@ -26,18 +37,39 @@ import xgboost as xgb
 #imports numpy-
 import numpy as np
 
-# In[cell2]
+# %%
 #reads in the census data
 census_data = pd.read_csv("adult.csv")
 
 #create a new column with a binary flag of "greater than 5ok or less than or equal to 50k"
+#%%
+#no longer needed with label encoding
+#census_data['greater_than_50k']=np.where(census_data['income'] == '>50K',1,0)
 
-census_data['greater_than_50k']=np.where(census_data['income'] == '>50K',1,0)
+#drops the education column because it is labeled with numbers
+#census_data.drop(['education'],axis = 1)
 
-census_data.head(15)
+#create a boolean mask for categorical colums
+cat_mask = (census_data.dtypes == 'object')
 
-census_data.info()
+#creates a OneHotEncoder from the cat mask booleans
+ohe = OneHotEncoder(categorical_features = cat_mask, sparse = False)
 
+#get a list of categorical column names
+cat_col = census_data.columns[cat_mask].tolist()
+
+#apply LabelEncoder to categorical columns
+census_data[cat_col] = census_data[cat_col].apply(lambda x: le.fit_transform(x))
+
+#drops old number for identifying education
+census_data.drop('education.num',axis=1)
+
+# %%
+#apply one OneHotEncoder to categorical columns, note the output is NO LONGER
+#a dataframe
+census_data_encoded = ohe.fit_transform(census_data)
+
+#%%
 #create target variable and features dataframes
 X,y = census_data.iloc[:,:-2],census_data.iloc[:,-1]
 
@@ -47,11 +79,10 @@ X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = 0.15, rando
 #create XGB Classifier
 xg_cl = xgb.XGBClassifier(objective = 'binary: logistic', n_estimators = 10, seed = 12345)
 
+#%%
 xg_cl.fit(X_train, y_train)
 
-y_train.head(10)
-X_train.head(10)
-
+#%%
 preds = xg_cl.predict(X_test)
 
 accuracy = float(np.sums(preds==y_test))/y_test.shape[0]
